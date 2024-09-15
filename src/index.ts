9a3737072cba8f7ddf0a5a9af5932e3fef5db579
Joshtri <stuffofyos1516@gmail.com>
2024-09-15 22:44:32 +0800

    updateing .ts format

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 
-import connectDB from './config/dbConfig';
-import guestRoute from './routes/guest.route';
+import connectDB from './config/dbConfig.ts';
+import guestRoute from './routes/guest.route.ts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
